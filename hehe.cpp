9261a81a63d16897054aa74ff2bdb9f36fed61d8
Sourabh Singh <specious0027@gmail.com>
2025-03-16 22:12:30 +0530

    week3 Done

--- hehe.cpp
+++ hehe.cpp
@@ -117,15 +117,95 @@ namespace week2{
     }
 }
 
-int main() {
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int key = 3;
-    // week1::q1(arr, key);
-    // week1::q2(arr, key);
-    // week1::q3(arr, key);
-    week2::q1(arr, key);
-    week2::q2(arr);
-    week2::q3(arr, key);
-    return 0;
+
+//? Main function for week 1 - 2
+
+// int main() {
+//     vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int key = 3;
+//     // week1::q1(arr, key);
+//     // week1::q2(arr, key);
+//     // week1::q3(arr, key);
+//     week2::q1(arr, key);
+//     week2::q2(arr);
+//     week2::q3(arr, key);
+//     return 0;
+// }
+
+#define ln "\n"
+#define ll long long
+
+namespace week3{
+    void q1(vector<int> v){
+        int cnt = 0;
+        if(v.size() == 0){
+            cout << "Empty array" << ln;
+            return;
+        }
+        for(int i = 1; i < v.size(); i++){
+            int key = v[i]; 
+            int j = i-1;
+            while(j>=0 && v[j] > key){
+                cnt++;
+                v[j+1] = v[j]; 
+                j--;
+            }
+            if(j>=0) cnt++; 
+            v[j+1] = key;
+        }
+        cout << "Number of comparisons: " << cnt << ln;
+        for(int i = 0; i < v.size(); i++){
+            cout << v[i] << " ";
+        }cout << ln;
+    }
+    void q2(vector<int> v){
+        int cnt = 0;
+        if(v.size() == 0){
+            cout << "Empty array" << ln;
+            return;
+        }
+        
+        for(int i = 0; i < v.size()-1; i++){
+            int min_idx = i;
+            for(int j = i+1; j < v.size(); j++){
+                cnt++;
+                if(v[j] < v[min_idx]){
+                    min_idx = j;
+                }
+            }
+            if(min_idx != i){
+                swap(v[min_idx], v[i]);
+            }
+        }
+        cout << "Number of comparisons: " << cnt << ln;
+        for(int i = 0; i < v.size(); i++){
+            cout << v[i] << " ";
+        }cout << ln;
+    }
+    void q3(vector<int> v) {
+        if(v.size() == 0) {
+            cout << "Empty array" << ln;
+            return;
+        }
+        sort(v.begin(), v.end());
+        bool hasDuplicate = false;
+        for(int i = 1; i < v.size(); i++) {
+            if(v[i] == v[i-1]) {
+                hasDuplicate = true;
+                break;
+            }
+        }
+        cout << (hasDuplicate ? "YES" : "NO") << ln;
+    }
 }
+
+//? main function for week 3
+
+// int main(){
+//     vector<int> v = {3,3,2,6,91,5,10,12};
+//     week3::q1(v);
+//     week3::q2(v);
+//     week3::q3(v);
+//     return 0;
+// }
 
