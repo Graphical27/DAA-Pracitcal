13681a9b5894c7ef2cc37876215e50a465f6aed8
Sourabh Singh <specious0027@gmail.com>
2025-03-17 07:34:24 +0530

    week4 done

--- hehe.cpp
+++ hehe.cpp
@@ -209,3 +209,143 @@ namespace week3{
 //     return 0;
 // }
 
+
+namespace week4{
+    void merge(vector<int>& arr,int left,int mid, int right, int& comp, int& inv){
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        vector<int> L(n1),R(n2);
+        for(int i = 0; i < n1; i++) L[i] = arr[left + i];
+        for(int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while(i < n1 && j < n2){
+            comp++;
+            if(L[i]<=R[j]){
+                arr[k] = L[i];
+                i++;
+                k++;
+            }else{
+                arr[k++] = R[j++];
+                inv+= (n1 - i);
+            }
+        }
+        while(i < n1){
+            arr[k++] = L[i++];
+        }
+        while(j < n2){
+            arr[k++] = R[j++];
+        }
+    }
+
+    void mergesort(vector<int>& arr,int left, int right, int& comp, int& inv){
+        if(left < right){
+            int mid = left + (right - left) / 2;
+            mergesort(arr,left,mid,comp,inv);
+            mergesort(arr,mid+1,right,comp,inv);
+            merge(arr,left,mid,right,comp,inv);
+        }
+    }
+
+    void q1(vector<int> arr){
+        int comp = 0, inv = 0;
+        if(arr.size() == 0){
+            cout << "Empty Array" << ln;
+            return;
+        }
+        mergesort(arr,0,arr.size()-1,comp,inv);
+        cout << "Sorted Array: ";
+        for(auto i:arr) cout << i << " ";
+        cout << ln;
+        cout << "Comp" << comp << " Inversion: " << inv << ln;
+    }
+    int partition(vector<int>& arr, int low, int high, int& comparisons, int& swaps) {
+        // Choose random pivot
+        int random = low + rand() % (high - low + 1);
+        swap(arr[random], arr[high]);
+        swaps++;
+        
+        int pivot = arr[high];
+        int i = low - 1;
+        
+        for(int j = low; j < high; j++) {
+            comparisons++;
+            if(arr[j] <= pivot) {
+                i++;
+                swap(arr[i], arr[j]);
+                swaps++;
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        swaps++;
+        return i + 1;
+    }
+    
+    void quickSort(vector<int>& arr, int low, int high, int& comparisons, int& swaps) {
+        if(low < high) {
+            int pi = partition(arr, low, high, comparisons, swaps);
+            
+            quickSort(arr, low, pi - 1, comparisons, swaps);
+            quickSort(arr, pi + 1, high, comparisons, swaps);
+        }
+    }
+    
+    void q2(vector<int> v) {
+        if(v.size() == 0) {
+            cout << "Empty array" << ln;
+            return;
+        }
+        
+        int comparisons = 0, swaps = 0;
+        srand(time(0)); // Seed random number generator
+        
+        quickSort(v, 0, v.size()-1, comparisons, swaps);
+        
+        cout << "Sorted array: ";
+        for(int i = 0; i < v.size(); i++) {
+            cout << v[i] << " ";
+        }
+        cout << ln;
+        cout << "Number of comparisons: " << comparisons << ln;
+        cout << "Number of swaps: " << swaps << ln;
+    }
+    void q3(vector<int> v,int k){
+        if(k < 0 || k > v.size()){
+            cout << "Invalid k value" << ln;
+            return;
+        }
+        priority_queue<int, vector<int> , greater<int>> minHeap;
+        priority_queue<int> maxHeap;
+
+        for(auto i:v){
+            minHeap.push(i);
+            maxHeap.push(i);
+        }
+        int kthsmallest;
+        for(int i = 0; i< k; i++){
+            kthsmallest = minHeap.top();
+            minHeap.pop();
+        }
+        int kthlargest;
+        for(int i = 0; i< k; i++){
+            kthlargest = maxHeap.top();
+            maxHeap.pop();
+        }
+        cout << "smallest: " << kthsmallest << " Largest: " << kthlargest << ln;
+    }
+}
+
+
+//? main function for week4
+// int main(){
+//     vector<int> v = {3,3,2,6,91,5,10,12};
+//     week4::q1(v);
+//     week4::q2(v);
+//     week4::q3(v,3);
+// }
+
+namespace week5{
+    
+}
+
+
